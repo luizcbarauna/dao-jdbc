be72598071b16d166968f1ab2a0a62fd328a7393
luizcbarauna <luizf.barauna@gmail.com>
2024-05-18 17:53:02 -0300

    Seller entity class

--- src/Program.java
+++ src/Program.java
@@ -1,4 +1,7 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,5 +9,8 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1,"books");
         System.out.println(obj);
+
+    Seller seller = new Seller(21,"Bob","Bob@gmail.com",new Date(),3000.0, obj);
+    System.out.println(seller);
     }
 }
