3861d51657b765835e11f3cd5533449c9618b89c
luizcbarauna <luizf.barauna@gmail.com>
2024-05-18 17:43:52 -0300

    Start the project

---
+++ src/Program.java
@@ -0,0 +1,10 @@
+import model.entities.Department;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Program {
+    public static void main(String[] args) {
+        Department obj = new Department(1,"books");
+        System.out.println(obj);
+    }
+}
